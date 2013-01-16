a26ef28a865a22d6cfbedd009a58c1cbdc90a14a
qianjun <er6788@126.com>
2013-01-16 13:32:24 +0800

    qianjun

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LantanBam::Application.config.secret_token = '7e7318c8e81e7cf8d8b8b206cf160c18eede4459719a308f3d115fc1d885e275e49db1fb39821950960215174d82747d4760cf7d21651722862943e9d681cdc4'
