1e68c2ea9852d96247006bee160b0e5962d1a53c
Ruby <alecchyi@gmail.com>
2013-01-29 10:01:46 +0800

    22
    2

--- app/models/material.rb
+++ app/models/material.rb
@@ -5,4 +5,7 @@ class Material < ActiveRecord::Base
   has_many :mat_out_orders
   has_many  :mat_in_orders
   has_many :prod_mat_relations
+
+  STATUS = {:normal => 0, :delete => 1}
+  TYPES = [["施工耗材类",0],["劳动保护",1],["工具类",2]]
 end
