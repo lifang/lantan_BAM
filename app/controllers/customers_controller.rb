0fbba8db758ef98a03beae15b6040173ab3ad47e
lifang <santhe_2004@126.com>
2013-01-29 16:22:35 +0800

    update customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 class CustomersController < ApplicationController
   def index
+    base_sql = "select c.id, c.name, c.phone, c.is_vip from customers c "
     
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,8 +6,8 @@ class Customer < ActiveRecord::Base
   has_many :revisits, :foreign_key => "user_id"
   has_many :send_messages
   has_many :c_svc_relations
- #客户类型
-  IS_VIP = {:normal=>0,:vip=>1} #0 常态客户 1 会员卡客户
 
-  TYPES = {:good=>1,:normal=>2,:key=>3} #1优质客户 2一般客户 3 重点客户
+ #客户类型
+  IS_VIP = {:normal => 0, :vip => 1} #0 常态客户 1 会员卡客户
+  TYPES = {:good => 1, :normal => 2, :stress => 3} #1 优质客户  2 一般客户  3 重点客户
 end
