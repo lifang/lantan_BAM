c54ea66c4d9befba0aafb84e4a0f9f10d5261a4f
Ruby <alecchyi@gmail.com>
2013-02-26 09:38:52 +0800

    update supplier

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,9 @@ LantanBAM::Application.routes.draw do
     end
     resources :materials do
       collection do
-        get "out","search","order","page_materials","search_head_orders","search_supplier_orders"
-        post "out_order","material_order","add"
+        get "out","search","order","page_materials","search_head_orders","search_supplier_orders","alipay",
+            "print","cuihuo","cancel_order","page_outs","page_ins","page_head_orders","page_supplier_orders","search_supplier_orders"
+        post "out_order","material_order","add","alipay_complete"
       end
     end
 
@@ -25,6 +26,9 @@ LantanBAM::Application.routes.draw do
     resources :suppliers do
       member do
         post "change"
+      end
+      collection do
+        get "page_suppliers"
       end
     end
     resources :welcomes
@@ -53,7 +57,7 @@ LantanBAM::Application.routes.draw do
       get "remark","check"
     end
     collection do
-      get "get_act_count", "out"
+      get "get_act_count", "out","order_remark"
     end
   end
 
