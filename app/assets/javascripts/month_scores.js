d78157cf70f9410a95644e31ed4a56fce5297631
qianjun <er6788@126.com>
2013-03-14 12:49:58 +0800

    qianjun

--- app/assets/javascripts/month_scores.js
+++ app/assets/javascripts/month_scores.js
@@ -1,2 +1,10 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function check_goal(){
+    var created  =$("#created").val();
+    var ended =$("#ended").val();
+    if ((created=="" || created.length==0) && (ended=="" || ended.length )){
+        alert("请选择目标销售额的起止日期！")
+    }
+    if(confirm("目标销售额不能更改，您确定创建该目标吗？")){
+        $("#create_goal").submit();
+    }
+}
