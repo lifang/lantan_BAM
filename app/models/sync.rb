6973e87a881e064c454a21c90fbc24d3e9a6f36a
cloud <cloud@cloud-Sony.(none)>
2013-04-23 13:49:06 +0800

    modify bug

--- app/models/sync.rb
+++ app/models/sync.rb
@@ -54,8 +54,6 @@ class Sync < ActiveRecord::Base
   end
 
 
-
-
   def self.out_data(store_id)
     path = Constant::LOCAL_DIR
     Dir.mkdir Constant::LOG_DIR  unless File.directory?  Constant::LOG_DIR
@@ -99,17 +97,16 @@ class Sync < ActiveRecord::Base
     flog.close
   end
 
-  def self.get_zip_file(day=1, sync, flog)
+  def self.get_zip_file(time, sync, flog, get_headoffice_zip_path)
     ip_host = Constant::HEAR_OFFICE_IPHOST
     path = Constant::LOCAL_DIR
-    dirs = ["syncs_datas/", "#{Time.now.ago(day).strftime("%Y-%m").to_s}/", "#{Time.now.ago(day).strftime("%Y-%m-%d")}/"]
-    read_dirs = ["write_datas/", "#{Time.now.ago(day).strftime("%Y-%m").to_s}/", "#{Time.now.ago(day).strftime("%Y-%m-%d")}/"]
+    read_dirs = ["write_datas/", "#{time.strftime("%Y-%m").to_s}/", "#{time.strftime("%Y-%m-%d")}/", "#{time.strftime("%H")}/"]
     read_dirs.each_with_index {|dir,index| Dir.mkdir path+read_dirs[0..index].join   unless File.directory? path+read_dirs[0..index].join }
     #Dir.mkdir dirs.join unless File.directory? dirs.join
-    file_name = "shared.zip"
+    file_name = "download.zip"
     is_download = false
     File.open(path+read_dirs.join+file_name, 'wb') do |fo|
-      fo.print open(ip_host+dirs.join+file_name).read
+      fo.print open(ip_host+get_headoffice_zip_path).read
       is_download = true
     end
     if is_download
@@ -124,6 +121,7 @@ class Sync < ActiveRecord::Base
 
   def self.output_zip(path, sync, flog)
     is_update = false
+    store_id = Store.all.first.id
     begin
       Zip::ZipFile.open(path){ |zipFile|
         zipFile.each do |file|
@@ -149,6 +147,8 @@ class Sync < ActiveRecord::Base
     rescue
       flog.write("当前目录文件#{path}更新失败---#{Time.now}\r\n")
     end
+    Reservation.destroy_all("store_id != #{store_id}")
+    Sale.update_all("store_id = #{store_id}")
     if is_update
       sync.update_attributes(:sync_status=>Sync::SYNC_STAT[:COMPLETE])
       flog.write("数据同步成功---#{Time.now}\r\n")
@@ -158,24 +158,20 @@ class Sync < ActiveRecord::Base
     end
   end
 
-  def self.request_is_generate_zip(day=0)  #发送请求，看是否已经生成zip文件
+  def self.request_is_generate_zip(time)  #发送请求，看是否已经生成zip文件
     url = Constant::HEAD_OFFICE_REQUEST_ZIP
-    
+
     Dir.mkdir Constant::LOG_DIR  unless File.directory?  Constant::LOG_DIR
     flog = File.open(Constant::LOG_DIR+Time.now.strftime("%Y-%m").to_s+".log","a+")
 
     sync =Sync.where("created_at='#{Time.now.strftime("%Y%m%d")}' and types=#{Sync::SYNC_TYPE[:SETIN]}")[0]
     sync =Sync.create(:created_at=>Time.now.strftime("%Y-%m-%d"),:types=>Sync::SYNC_TYPE[:SETIN]) if sync.nil?
     result = Net::HTTP.get(URI.parse(url))
-    puts "***********"
-    puts result.inspect
-    puts "***************"
-    if result == "complete"
-      get_zip_file(day, sync, flog)
-    else
+    if result == "uncomplete"
       flog.write("zip文件还没有生成成功---#{Time.now}\r\n")
+    else
+      get_zip_file(time, sync, flog, result)
     end
-    
   end
   
 end
