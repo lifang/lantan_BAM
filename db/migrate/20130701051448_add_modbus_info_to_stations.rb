d24f9f73dce1f232639fa31557c831878b7f48b8
lifang <santhe_2004@126.com>
2013-04-26 17:39:44 +0800

    update detial

--- db/migrate/20130701051448_add_modbus_info_to_stations.rb
+++ db/migrate/20130701051448_add_modbus_info_to_stations.rb
@@ -6,7 +6,12 @@ class AddModbusInfoToStations < ActiveRecord::Migration
     add_column :stations, :gas_run_fb, :boolean #空气机运行反馈
     add_column :stations, :gas_error_fb, :boolean #空气机故障反馈
     add_column :stations, :system_error, :boolean #系统报警
-    add_column :stations, :is_using, :boolean #工位有些占用
+    add_column :stations, :is_using, :boolean #工位有效占用
+    add_column :stations, :day_hmi, :boolean #工位日hmi复位
+    add_column :stations, :month_hmi, :boolean #工位月hmi复位
+    add_column :stations, :once_gas_use, :boolean #工位一次使用的气体数量
+    add_column :stations, :once_water_use, :boolean #工位一次使用的水数量
+    add_column :work_orders, :gas_num, :boolean
     
   end
 end
