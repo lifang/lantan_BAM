f2c7f42197ecc411640025dfe8c1d4b2c4a77a59
孟凡玲 <mengfanling825@163.com>
2013-04-27 09:28:18 +0800

    restore

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -366,7 +366,7 @@ class MaterialsController < ApplicationController
                 order.update_attribute(:status, MaterialOrder::STATUS[:pay])
                 if order.supplier_type==0
                   headoffice_post_api_url = Constant::HEAD_OFFICE_API_PATH + "api/materials/update_status"
-                  result = Net::HTTP.post_form(URI.parse(headoffice_post_api_url), {'mo_code' => order.code, 'status' => MaterialOrder::STATUS[:pay]})
+                  result = Net::HTTP.post_form(URI.parse(headoffice_post_api_url), {'mo_code' => order.code, 'mo_status' => MaterialOrder::STATUS[:pay]})
                 end
                 #支付记录
                 MOrderType.create(:material_order_id => order.id,:pay_types => MaterialOrder::PAY_TYPES[:CHARGE], :price => order.price)
@@ -427,7 +427,7 @@ class MaterialsController < ApplicationController
       if order && order.status == MaterialOrder::STATUS[:no_pay] && order.m_status == MaterialOrder::M_STATUS[:no_send]
         order.update_attribute(:status,MaterialOrder::STATUS[:cancel])
         headoffice_post_api_url = Constant::HEAD_OFFICE_API_PATH + "api/materials/update_status"
-        result = Net::HTTP.post_form(URI.parse(headoffice_post_api_url), {'mo_code' => order.code, 'status' => MaterialOrder::STATUS[:cancel]})
+                  result = Net::HTTP.post_form(URI.parse(headoffice_post_api_url), {'mo_code' => order.code, 'mo_status' => MaterialOrder::STATUS[:cancel]})
       elsif order.status == MaterialOrder::STATUS[:cancel]
         content = "订单已取消"
       else
@@ -488,7 +488,7 @@ class MaterialsController < ApplicationController
           @current_store.update_attribute(:account, @current_store.account - @mat_order.price) if @current_store
         end
         headoffice_post_api_url = Constant::HEAD_OFFICE_API_PATH + "api/materials/update_status"
-        result = Net::HTTP.post_form(URI.parse(headoffice_post_api_url), {'mo_code' => @mat_order.code, 'status' => MaterialOrder::STATUS[:pay]})
+        result = Net::HTTP.post_form(URI.parse(headoffice_post_api_url), {'mo_code' => @mat_order.code, 'mo_status' => MaterialOrder::STATUS[:pay]})
         p "----------------------------------"
         p result
         render :json => {:status => 0}
