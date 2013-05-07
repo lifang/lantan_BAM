d17401bd540a35719e2f855db2b821055426bbfb
qianjun <er6788@126.com>
2013-05-07 14:28:03 +0800

    qianjun

--- app/controllers/syncs_controller.rb
+++ app/controllers/syncs_controller.rb
@@ -8,4 +8,21 @@ class SyncsController < ActionController::Base
     render :text=>"success"
   end
 
+  def upload_image
+    filename = params[:imgFile].original_filename
+    time = Time.now.strftime("%Y%m%d")
+    dir = "#{File.expand_path(Rails.root)}/public/upload_images"
+    Dir.mkdir(dir) unless File.directory?(dir)
+    Dir.mkdir("#{dir}/#{time}") unless File.directory?("#{dir}/#{time}")
+    File.open("#{dir}/#{time}/#{filename}", "wb") do |f|
+      f.write(params[:imgFile].read)
+    end
+    respond_to do |format|
+      format.json {
+        data={:error=>0, :url=>"/upload_images/#{time}/#{filename}", :message=>"upload_image error"}
+        render :json=>data
+      }
+    end
+  end
+
 end
