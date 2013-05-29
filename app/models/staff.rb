4b5162a2edfd1035869e18b55d3a2f0b258e487b
cloud <cloud@cloud-Sony.(none)>
2013-05-29 13:21:37 +0800

    modify bug

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -76,6 +76,15 @@ class Staff < ActiveRecord::Base
     end
   end
 
+  def self.update_staff_working_stats
+    Staff.where("working_stats = 0").each do |staff|
+      diff_day = (Time.now - staff.created_at).to_i / (24 * 60 * 60)
+      if diff_day == staff.probation_days
+        staff.update_attribute(:working_stats, 1)
+      end
+    end
+  end
+
   private
   def encrypt(string)
     self.salt = make_salt if new_record?
