2f7cd85e65c8ac76d30ac753907c9e9d89ad217c
Luping <165340815@qq.com>
2013-05-29 10:00:31 +0800

    luping05293

--- db/migrate/20130701051454_add_working_stats_and_probation_salary_and_is_deduct_and_probation_days_to_staffs.rb
+++
@@ -1,8 +0,0 @@
-class AddWorkingStatsAndProbationSalaryAndIsDeductAndProbationDaysToStaffs < ActiveRecord::Migration
-  def change
-    add_column :staffs, :working_stats, :int    #在职状态 0试用 1正式
-    add_column :staffs, :probation_salary, :float   #试用薪资
-    add_column :staffs, :is_deduct, :boolean        #是否提成
-    add_column :staffs, :probation_days, :int       #试用期(天)
-  end
-end
