284bd6afca85ef7ba907b147db0adda9ed88956f
cloud <cloud@cloud-Sony.(none)>
2013-05-29 15:51:07 +0800

    modify bug

--- app/helpers/market_manages_helper.rb
+++ app/helpers/market_manages_helper.rb
@@ -1,8 +1,12 @@
 module MarketManagesHelper
   def get_service_products(order)
-    Product.find_by_sql("select p.name, p.id, op.price price,op.pro_num pro_num from products p inner join
+    products = Product.find_by_sql("select p.name name, p.id, op.price price,op.pro_num pro_num from products p inner join
                         order_prod_relations op on op.product_id = p.id inner join orders o on o.id=op.order_id
                         where o.id=#{order.id}")
+    pcar_relations = CPcardRelation.find_by_sql("select pc.price price, pc.name name, 1 pro_num 
+        from c_pcard_relations cpr inner join package_cards pc
+        on pc.id = cpr.package_card_id where cpr.order_id = #{order.id}")
+    products + pcar_relations
     #p.is_service=#{Product::PROD_TYPES[:SERVICE]} 
   end
 
