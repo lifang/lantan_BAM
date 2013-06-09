3ff38232a9d0bbc6d3d9e647131813fb35ee313e
孟凡玲 <mengfanling825@163.com>
2013-06-09 16:16:58 +0800

    buy save_cards

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -170,7 +170,7 @@ class Api::OrdersController < ApplicationController
       if order && order.status == Order::STATUS[:NORMAL]
         oprs = OPcardRelation.find_all_by_order_id(order.id)
         oprs.each do |opr|
-          cpr = CPcardRelation.find(opr.c_pcard_relation_id)
+          cpr = CPcardRelation.find_by_id(opr.c_pcard_relation_id)
           pns = cpr.content.split(",").map{|pn| pn.split("-")} if cpr
           pns.each do |pn|
             pn[2] = pn[2].to_i + opr.product_num if pn[0].to_i == opr.product_id
@@ -258,5 +258,46 @@ class Api::OrdersController < ApplicationController
     end
     resp_text = flag ? "success" : "error"
     render :json => {:status => resp_text}
+  end
+
+  #发送短信code
+  def get_user_svcard
+    record = CSvcRelation.find_by_sql(["select csr.* from c_svc_relations csr
+      left join lantan_db_all.customers c on c.id = csr.customer_id where c.mobilephone = ?",
+        params[:mobilephone].strip])[0]
+    status = 0
+    send_message = "余额不足，您的储值卡余额为#{record.left_price}元。" if record
+
+    if record and  record.left_price >= params[:price].to_f
+      record.verify_code = proof_code(6)
+      record.save
+      status = 1
+      send_message = "感谢您使用澜泰储值卡，您本次的消费验证码为：#{record.verify_code}。"
+      message_route = "/send.do?Account=#{Constant::USERNAME}&Password=#{Constant::PASSWORD}&Mobile=#{params[:mobilephone].strip}&Content=#{send_message}&Exno=0"
+      create_get_http(Constant::MESSAGE_URL, message_route)
+      message = "发送成功。"
+    elsif record.nil?
+      message = "账号不存在。"
+    end
+    render :json => {:content => message, :status => status}
+  end
+
+  #使用储值卡支付
+  def use_svcard
+    record = CSvcRelation.find_by_sql(["select csr.* from c_svc_relations csr
+      left join lantan_db_all.customers c on c.id = csr.customer_id where c.mobilephone = ? and csr.verify_code = ?",
+        params[:mobilephone].strip, params[:verify_code].strip])[0]
+    status = 0
+    message = "支付失败。"
+    if record and  record.left_price >= params[:price].to_f
+      left_price = record.left_price - params[:price].to_f
+      SvcardUseRecord.create(:c_svc_relation_id => record.id, :types => SvcardUseRecord::TYPES[:OUT],
+        :use_price => params[:price].to_f, :left_price => left_price, :content => params[:content].strip)
+      record.left_price = left_price
+      record.save
+      status = 1
+      message = "支付成功。"
+    end
+    render :json => {:content => message, :status => status}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -207,7 +207,7 @@ LantanBAM::Application.routes.draw do
     resources :orders do
       collection do
         post "login","add","pay","complaint","search_car","send_code","index_list","brands_products","finish",
-          "confirm_reservation","refresh","pay_order","checkin", "show_car", "sync_orders_and_customer"
+          "confirm_reservation","refresh","pay_order","checkin", "show_car", "sync_orders_and_customer","get_user_svcard","use_svcard"
       end
     end
     resources :syncs_datas do
