a431f999261a79afebe201298c027de2fb7c3f53
cloud <cloud@cloud-Sony.(none)>
2013-06-19 14:42:21 +0800

    modify bug

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -23,9 +23,9 @@ class WorkOrder < ActiveRecord::Base
               station.update_attribute(:status, Station::STAT[:WRONG])
             else
               station.update_attribute(:status, Station::STAT[:LACK]) if station.status == Station::STAT[:WRONG]
-              work_order = WorkOrder.where("status = #{WorkOrder::STAT[:SERVICING]} and station_id = #{station.id}").first
+              work_order = WorkOrder.where("status = #{WorkOrder::STAT[:SERVICING]} and station_id = #{station.id} and current_day = #{current_day}").first
               started_at_sql = work_order.nil? ? "1=1" : "started_at >= '#{work_order.started_at}'"
-              next_work_order = WorkOrder.where("status = #{WorkOrder::STAT[:WAIT]} and station_id = #{station.id}").where(started_at_sql).order("started_at asc").first
+              next_work_order = WorkOrder.where("status = #{WorkOrder::STAT[:WAIT]} and station_id = #{station.id} and current_day = #{current_day}").where(started_at_sql).order("started_at asc").first
               work_order.update_attributes(:status => WorkOrder::STAT[:COMPLETE],
                 :water_num => data_arr[3], :gas_num => data_arr[4]) if work_order
               next_work_order.update_attribute(:status, WorkOrder::STAT[:SERVICING]) if next_work_order
