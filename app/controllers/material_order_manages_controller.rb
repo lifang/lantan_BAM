3e7ee7a15c0568312e8040314df88737990c10d7
cloud <cloud@cloud-Sony.(none)>
2013-06-20 15:01:29 +0800

    modify bug

--- app/controllers/material_order_manages_controller.rb
+++ app/controllers/material_order_manages_controller.rb
@@ -5,15 +5,9 @@ class MaterialOrderManagesController < ApplicationController
 
   def index
     @store = Store.find_by_id(params[:store_id])
-
     @statistics_month = (params[:statistics_month] ||= Time.now.months_ago(1).strftime("%Y-%m"))
-
-    arrival_at_sql = "arrival_at >= '#{@statistics_month}-01' and arrival_at <= '#{@statistics_month}-31'"
-
-    @material_orders = MaterialOrder.
-                    where("store_id = #{params[:store_id]}").
-                    where(arrival_at_sql)
-
+    arrival_at_sql = "arrival_at>='#{@statistics_month}-01' and date_format(arrival_at,'%Y-%m-%d')<='#{@statistics_month}-31'"
+    @material_orders = MaterialOrder.where("store_id = #{params[:store_id]}").where(arrival_at_sql)
     @total_price = @material_orders.sum(:price)
   end
 
