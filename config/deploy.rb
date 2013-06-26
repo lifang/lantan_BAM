46b3a83da1caffdaeca9de967012f08ba8a474cc
cloud <cloud@cloud-Sony.(none)>
2013-06-26 15:54:11 +0800

    modify bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,13 @@
 set :use_sudo, false
 set :application, "lantan_BAM"
-
+default_run_options[:pty] = true
 set :scm, :git
+set :ssh_options, { :forward_agent => true } 
 set :repository,  "git@github.com:lifang/lantan_BAM.git"
-set :git_shallow_clone, 1
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-default_run_options[:pty] = true
+
 set :user, "root"
 set :deploy_to, "/opt/projects/#{application}"
 set :current_path, "#{deploy_to}/current"
@@ -17,25 +18,7 @@ role :app, "192.168.0.250"                          # This may be the same as yo
 role :db,  "192.168.0.250", :primary => true # This is where Rails migrations will run
 set :rails_env, 'production'
 
-
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
-
 namespace :deploy do
-
   task :start, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
@@ -44,15 +27,4 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
     run "cd #{deploy_to}/current;rake assets:precompile"
   end
-
-#  task :after_symlink, :roles => :app do
-#
-#    # database.yml for localized database connection
-#    run "rm #{current_path}/config/database.yml"
-#    run "ln -s #{shared_path}/database.yml #{current_path}/config/database.yml"
-#
-#    # log link
-#    run "rm #{current_path}/log"
-#    run "ln -s #{shared_path}/log/ #{current_path}/log"
-#  end
 end
