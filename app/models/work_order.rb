939cf5936688740798868c8fcae7d3c5be3f2436
cloud <cloud@cloud-Sony.(none)>
2013-07-12 17:53:33 +0800

    modify has ipc sort station function

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -7,6 +7,7 @@ class WorkOrder < ActiveRecord::Base
   STAT = {:WAIT =>0,:SERVICING=>1,:WAIT_PAY=>2,:COMPLETE=>3, :CANCELED => 4}
 
   def self.update_work_order
+    current_time = Time.now
     current_day = Time.now.strftime("%Y%m%d")
     file_path = Constant::WORK_ORDER_PATH+current_day+".txt"
     if File.exist?(file_path)
@@ -23,12 +24,13 @@ class WorkOrder < ActiveRecord::Base
               station.update_attribute(:status, Station::STAT[:WRONG])
             else
               station.update_attribute(:status, Station::STAT[:LACK]) if station.status == Station::STAT[:WRONG]
-              work_order = WorkOrder.where("status = #{WorkOrder::STAT[:SERVICING]} and station_id = #{station.id} and current_day = #{current_day}").first
+              work_order = WorkOrder.where("status = #{WorkOrder::STAT[:SERVICING]} and station_id = #{station.id} and current_day = #{current_day} and store_id = #{station.store_id}").first
               started_at_sql = work_order.nil? ? "1=1" : "started_at >= '#{work_order.started_at}'"
-              next_work_order = WorkOrder.where("status = #{WorkOrder::STAT[:WAIT]} and station_id = #{station.id} and current_day = #{current_day}").where(started_at_sql).order("started_at asc").first
+              next_work_order = WorkOrder.where("status = #{WorkOrder::STAT[:WAIT]} and station_id = #{station.id} and current_day = #{current_day} and store_id = #{station.store_id}").where(started_at_sql).order("started_at asc").first
               if work_order
+                runtime = sprintf('%.2f',(current_time - work_order.started_at)/60).to_f
                 work_order.update_attributes(:status => WorkOrder::STAT[:WAIT_PAY],
-                  :water_num => data_arr[3], :gas_num => data_arr[4])
+                  :water_num => data_arr[3], :gas_num => data_arr[4], :runtime => runtime)
                 order = work_order.order
                 order.update_attribute(:status, Order::STATUS[:WAIT_PAYMENT]) if order
               end
@@ -41,8 +43,7 @@ class WorkOrder < ActiveRecord::Base
           end
         end
         if equipment_info.nil?
-          store = Store.all.first
-          EquipmentInfo.create(:current_day => current_day.to_i, :num => file_data_arr.length, :store_id => store.try(:id))
+          EquipmentInfo.create(:current_day => current_day.to_i, :num => file_data_arr.length)
         else
           equipment_info.update_attribute(:num, file_data_arr.length)
         end
