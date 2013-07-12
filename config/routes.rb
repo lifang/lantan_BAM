05eabdb8830bc53156c53f7ada7e8f42b72e074b
cloud <cloud@cloud-Sony.(none)>
2013-07-12 18:59:37 +0800

    modify api

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ LantanBAM::Application.routes.draw do
   end
   resources :work_orders do
     collection do
-      get "work_orders_status", "work_order_finished"
+      get "work_orders_status"
     end
   end
   resources :package_cards do
@@ -232,7 +232,8 @@ LantanBAM::Application.routes.draw do
     resources :orders do
       collection do
         post "login","add","pay","complaint","search_car","send_code","index_list","brands_products","finish",
-          "confirm_reservation","refresh","pay_order","checkin", "show_car", "sync_orders_and_customer","get_user_svcard","use_svcard"
+          "confirm_reservation","refresh","pay_order","checkin", "show_car", "sync_orders_and_customer","get_user_svcard",
+          "use_svcard","work_order_finished","into_materials"
       end
     end
     resources :syncs_datas do
