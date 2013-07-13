65f2911f410adb145c2726e42658ef63016b1618
lifang <santhe_2004@126.com>
2013-07-13 10:33:14 +0800

    update detial

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -17,9 +17,9 @@ class Api::OrdersController < ApplicationController
   def login
     staff = Staff.find_by_username(params[:user_name])
     info = ""
-    if  staff.nil? or !staff.has_password?(params[:user_password]) or staff.store.status != Store::STATUS[:OPENED]
+    if  staff.nil? or !staff.has_password?(params[:user_password])
       info = "用户名或密码错误"
-    elsif !Staff::VALID_STATUS.include?(staff.status)
+    elsif !Staff::VALID_STATUS.include?(staff.status) or staff.store.status != Store::STATUS[:OPENED]
       info = "用户不存在"
     else
       cookies[:user_id]={:value => staff.id, :path => "/", :secure  => false}
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -21,12 +21,12 @@ class LoginsController < ApplicationController
 
   def create
     @staff = Staff.find_by_username(params[:user_name])
-    if  @staff.nil? or !@staff.has_password?(params[:user_password]) or @staff.store.status != Store::STATUS[:OPENED]
+    if  @staff.nil? or !@staff.has_password?(params[:user_password]) 
       flash.now[:notice] = "用户名或密码错误"
       #redirect_to "/"
       @user_name = params[:user_name]
       render 'index', :layout => false
-    elsif !Staff::VALID_STATUS.include?(@staff.status)
+    elsif !Staff::VALID_STATUS.include?(@staff.status) or @staff.store.status != Store::STATUS[:OPENED]
       flash.now[:notice] = "用户不存在"
       @user_name = params[:user_name]
       render 'index', :layout => false
