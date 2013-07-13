11674406643ae631ac2a9b5df2b4cbd151310d31
Luping <165340815@qq.com>
2013-07-13 18:56:54 +0800

    luping0713

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,6 +6,7 @@ class Role < ActiveRecord::Base
   has_many :role_menu_relations
   has_many :menus, :through => :role_menu_relations, :foreign_key => "menu_id"
 
+  ADMIN = "门店管理员"        #门店管理员的角色名
   ROLE_TYPE = {
     :NORMAL => 1,   #门店员工,
     :STORE_MANAGER => 0 #门店管理员
