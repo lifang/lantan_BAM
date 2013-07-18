4b93b2f1325fd92a29cefb08cd708ab0c8fe7dfd
CharlesZhu <zhuchuantao1990@126.com>
2013-07-18 10:58:50 +0800

    modify function materials_losses

--- app/controllers/materials_losses_controller.rb
+++ app/controllers/materials_losses_controller.rb
@@ -1,28 +1,21 @@
 #encoding:utf-8
 class MaterialsLossesController < ApplicationController
-   def add
-    if params[:id].empty?
-      material =  MaterialLoss.new({:name => params[:name].strip, :code => params[:code].strip,
-      :types => params[:types].strip, :price => params[:price].strip.to_i,
-      :sale_price => params[:sale_price].strip.to_i, :loss_num => params[:loss_num].strip.to_i,
-      :specifications => params[:specifications].strip, :staff_id => params[:report_person],
-      :store_id => params[:store_id]
-        }) if material.nil?
-
-      if material.save
-       redirect_to "/stores/#{params[:store_id]}/materials"
+  def add
+    mat_losses = params[:mat_losses]
+    mat_losses.each do |key,value|
+      material = Material.find(mat_losses[key][:mat_id])
+      unless material.nil?
+        MaterialLoss.create({:name => material.name, :code => material.code,
+                             :types => material.types, :price => material.price.to_i,
+                             :sale_price => material.sale_price.to_i, :loss_num =>  mat_losses[key][:mat_num].to_i,
+                             :specifications => material.unit, :staff_id => params[:staff],
+                             :store_id => params[:hidden_store_id]
+                             })
       end
-    else
-      material = MaterialLoss.find(params[:id])
-     if material.update_attributes({:name => params[:name].strip, :code => params[:code].strip,
-                                      :types => params[:types].strip, :price => params[:price].strip.to_i,
-                                      :sale_price => params[:sale_price].strip.to_i, :loss_num => params[:loss_num].strip.to_i,
-                                      :specifications => params[:specifications].strip, :staff_id => params[:report_person],
-                                     })
-       redirect_to "/stores/#{params[:store_id]}/materials"
-     end
     end
-   end
+    flash[:notice] = "报损成功"
+    redirect_to "/stores/#{params[:hidden_store_id]}/materials"
+  end
 
    def delete
      material =  MaterialLoss.find(params[:materials_loss_id].to_i)
