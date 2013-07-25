e1e38fef8a52a4bbb8140adb3efba8476f7eba27
qianjun <er6788@126.com>
2013-07-25 17:37:33 +0800

    qianjun

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -16,9 +16,10 @@ class StationsController < ApplicationController
       servs = work_orders[WorkOrder::STAT[:SERVICING]].nil? ? {} : work_orders[WorkOrder::STAT[:SERVICING]]
       servs.each { | a| @f_working[a.front_staff_id].nil? ? @f_working[a.front_staff_id]=[a] : @f_working[a.front_staff_id] << a}
       @nums = servs.inject(Hash.new) { |hash, a| hash.merge(a.station_id=>a.num)}
-      StationStaffRelation.find_by_sql("select staff_id t_id,station_id s_id from station_staff_relations where station_id in (#{@stations.map(&:id).join(',')})
-    and current_day='#{Time.now.strftime("%Y%m%d")}' ").each {|staff|@staff_ids[staff.s_id].nil? ? @staff_ids[staff.s_id]=[staff.t_id] : @staff_ids[staff.s_id]<<staff.t_id}
-      Staff.where("id in (#{@staff_ids.values.flatten.uniq.join(',')})").each{|staff|@staffs[staff.id]=staff.name} unless @staff_ids == {}
+      StationStaffRelation.find_by_sql("select staff_id t_id,station_id s_id from station_staff_relations s inner join staffs t on 
+     t.id=s.staff_id where station_id in (#{@stations.map(&:id).join(',')}) and status !=#{Station::STAT[:DELETED]} and current_day='#{Time.now.strftime("%Y%m%d")}' ").each {|staff|
+        @staff_ids[staff.s_id].nil? ? @staff_ids[staff.s_id]=[staff.t_id] : @staff_ids[staff.s_id]<<staff.t_id}
+      Staff.where("id in (#{@staff_ids.values.flatten.uniq.join(',')}) and status !=#{Station::STAT[:DELETED]}").each{|staff|@staffs[staff.id]=staff.name} unless @staff_ids == {}
       WorkOrder.where("store_id=#{params[:store_id]} and status=#{WorkOrder::STAT[:SERVICING]} and current_day=#{Time.now.strftime('%Y%m%d').to_i}").each{|work_order|
         @times[work_order.station_id]=work_order.ended_at.nil? ? 0 : (work_order.ended_at- Time.now) }
     end
