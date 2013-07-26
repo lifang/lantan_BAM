21ec8217b5fc049ec2546c38b5f3d9af4dc16c8b
cloud <cloud@cloud-Sony.(none)>
2013-07-26 11:19:42 +0800

    modify bug

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -143,16 +143,18 @@ class WorkOrder < ActiveRecord::Base
       if same_work_orders.any?
         product_ids = same_work_orders[0].order.order_prod_relations.map(&:product_id)
         infos = Station.return_station_arr(product_ids, same_work_orders[0].store_id)
+
         station_arr = infos[0]
         wkor_times = WorkOrder.where(:station_id => station_arr, :current_day => Time.now.strftime("%Y%m%d"),
                      :store_id =>self.store_id, :status => [WorkOrder::STAT[:WAIT], WorkOrder::STAT[:SERVICING]]).map(&:station_id)
+
         if station_arr.any? and (wkor_times.blank? or wkor_times.length < station_arr.length)
           leave_station = (station_arr - wkor_times)[0]
           s_ended_at = Time.now + same_work_orders[0].cost_time*60
-          same_work_orders[0].update_attributes(:status => WorkOrder::STAT[:SERVICING], :station_id => leave_station,
+          same_work_orders[0].update_attributes(:status => WorkOrder::STAT[:SERVICING], :station_id => leave_station.id,
             :started_at => Time.now, :ended_at => s_ended_at)
           same_work_orders[0].order.update_attribute(:status, Order::STATUS[:SERVICING]) if same_work_orders[0].order.status != Order::STATUS[:BEEN_PAYMENT]
-          WkOrTime.create(:current_day => Time.now.strftime("%Y%m%d").to_i, :station_id => leave_station,
+          WkOrTime.create(:current_day => Time.now.strftime("%Y%m%d").to_i, :station_id => leave_station.id,
             :current_times => s_ended_at.strftime("%Y%m%d%H%M"))
         end
       end
