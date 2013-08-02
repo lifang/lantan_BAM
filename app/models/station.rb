9c4247c841a1e0492d430991bad2dfb642743b60
amanda <mengfanling825@163.com>
2013-08-02 16:52:40 +0800

    stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -5,11 +5,7 @@ class Station < ActiveRecord::Base
   has_many :staffs, :through => :station_staff_relations
   has_many :station_service_relations
   has_many :wk_or_times
-  has_many :products, :through => :station_service_relations do
-    def valid
-      where("status=true and is_service=true")
-    end
-  end
+  has_many :products, :through => :station_service_relations
   belongs_to :store
   STAT = {:WRONG =>0,:NORMAL =>2,:LACK =>1,:NO_SERVICE =>3, :DELETED => 4} #0 故障 1 缺少技师 2 正常 3 无服务
   STAT_NAME = {0=>"故障",1=>"缺少技师",2=>"正常",3=>"缺少服务项目"}
