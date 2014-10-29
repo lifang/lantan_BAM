bceb0c94f479ca796b0b1414a8e3db9e0c318958
lifang <santhe_2004@126.com>
2014-10-29 14:29:41 +0800

    qianjun

--- db/migrate/20130701051450_add_verify_code_to_c_svc_relations.rb
+++
@@ -1,5 +0,0 @@
-class AddVerifyCodeToCSvcRelations < ActiveRecord::Migration
-  def change
-    add_column :c_svc_relations, :verify_code, :string
-  end
-end
--- db/migrate/20131126081735_add_password_to_c_svc_relations.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordToCSvcRelations < ActiveRecord::Migration
-  def change
-    add_column :c_svc_relations, :password, :string
-  end
-end
